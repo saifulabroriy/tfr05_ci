cbe788dc817c2222fc11b2d8d61378e4826ab687
saifulabroriy <saifulabroriy2000@gmail.com>
2022-03-23 08:18:41 +0700

    Memakai Myth/Auth untuk otentikasi

--- app/Config/Email.php
+++ app/Config/Email.php
@@ -9,12 +9,12 @@ class Email extends BaseConfig
     /**
      * @var string
      */
-    public $fromEmail;
+    public $fromEmail = "saifulabroriy4000@gmail.com";
 
     /**
      * @var string
      */
-    public $fromName;
+    public $fromName = "Saiful Abroriy";
 
     /**
      * @var string
