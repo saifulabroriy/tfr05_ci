46572d092acd1160825f676b8c4dbc703cd921b6
saifulabroriy <saifulabroriy2000@gmail.com>
2022-04-13 17:07:30 +0700

    Loggin CRUD Pelanggan

--- app/Controllers/Pelanggan.php
+++ app/Controllers/Pelanggan.php
@@ -4,14 +4,18 @@ namespace App\Controllers;
 
 use App\Controllers\BaseController;
 use App\Models\PelangganModel;
+use App\Models\LogUserModel;
 
 class Pelanggan extends BaseController
 {
+    protected $logUser;
     protected $pelanggan;
     protected $request;
 
     public function __construct()
     {
+        $this->logUser = new LogUserModel();
+        $this->logUser->protect(false);
         $this->pelanggan = new PelangganModel();
         $this->pelanggan->protect(false);
         $this->request = \Config\Services::request();
@@ -58,11 +62,31 @@ class Pelanggan extends BaseController
 
     public function store()
     {
+        $nama = $this->request->getPost('nama');
+        $alamat = $this->request->getPost('alamat');
+        $notelp = $this->request->getPost('notelp');
+
         $this->pelanggan->save([
-            'nama' => $this->request->getPost('nama'),
-            'alamat' => $this->request->getPost('alamat'),
-            'notelp' => $this->request->getPost('notelp'),
+            'nama' => $nama,
+            'alamat' => $alamat,
+            'notelp' => $notelp,
         ]);
+
+        $after = [
+            'nama' => $nama,
+            'alamat' => $alamat,
+            'notelp' => $notelp,
+        ];
+
+        $log = [
+            'iduser' => session()->get('id'),
+            'menu' => 'Pelanggan',
+            'keterangan' => 'Menambah pelanggan',
+            'before' => '',
+            'after' => json_encode($after),
+        ];
+
+        $this->logUser->insert($log);
 
         return redirect()->to('admin/pelanggan')->with('success', 'Pelanggan berhasil ditambahkan');
     }
@@ -78,16 +102,65 @@ class Pelanggan extends BaseController
 
     public function update($id)
     {
+        $pelanggan = $this->pelanggan->find($id);
+
+        $before = [
+            'nama' => $pelanggan['nama'],
+            'alamat' => $pelanggan['alamat'],
+            'notelp' => $pelanggan['notelp'],
+        ];
+
+        $nama = $this->request->getPost('nama');
+        $alamat = $this->request->getPost('alamat');
+        $notelp = $this->request->getPost('notelp');
+
         $this->pelanggan->update($id, [
-            'pelanggan' => $this->request->getPost('pelanggan'),
+            'nama' => $nama,
+            'alamat' => $alamat,
+            'notelp' => $notelp,
         ]);
+
+        $after = [
+            'nama' => $nama,
+            'alamat' => $alamat,
+            'notelp' => $notelp,
+        ];
+
+        $log = [
+            'iduser' => session()->get('id'),
+            'menu' => 'Pelanggan',
+            'keterangan' => 'Mengubah pelanggan',
+            'before' => json_encode($before),
+            'after' => json_encode($after),
+        ];
+
+        $this->logUser->insert($log);
 
         return redirect()->to('admin/pelanggan')->with('success', 'Pelanggan berhasil diubah');
     }
 
     public function delete($id)
     {
+        $pelanggan = $this->pelanggan->find($id);
+
+        $before = [
+            'nama' => $pelanggan['nama'],
+            'alamat' => $pelanggan['alamat'],
+            'notelp' => $pelanggan['notelp'],
+        ];
+
+        $log = [
+            'iduser' => session()->get('id'),
+            'menu' => 'Pelanggan',
+            'keterangan' => 'Menghapus pelanggan',
+            'before' => json_encode($before),
+            'after' => '',
+        ];
+
         $this->pelanggan->delete($id);
+
+        $this->logUser->insert($log);
+
         return redirect()->to('admin/pelanggan')->with('success', 'Pelanggan berhasil dihapus');
     }
 }
